4649661801ce0baba762499008d31566d4c5e053
Mihai Stan <117821906+MihaiS1988@users.noreply.github.com>
2023-03-06 13:27:19 +0000

    Added the code for each team member

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// This imports the Employee class
+const Employee = require('./Employee');
+
+// This will define the Engineer class that inherits from Employee
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return 'Engineer';
+  }
+}
+
+// This will export the Engineer class
+module.exports = Engineer;
+
--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// Import the Employee class
+const Employee = require('./Employee');
+
+// Define the Engineer class that inherits from Employee
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+  getGithub() {
+    return this.school;
+  }
+
+  getRole() {
+    return 'Intern';
+  }
+}
+
+// Export the Intern class
+module.exports = Intern;
+
